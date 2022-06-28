1b26c8bd580361d6561053b84573d0f46c38ac7e
Misael Vargas <misaelomar.1590@hotmail.com>
2022-06-28 14:49:36 -0500

    Add Auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+Route::get('/redirect', function (Request $request) {
+    $request->session()->put('state', $state = Str::random(40));
+
+    $query = http_build_query([
+        'client_id' => '3',
+        'redirect_uri' => 'flutter://',
+        'response_type' => 'code',
+        'scope' => '*',
+        'state' => $state,
+    ]);
+
+    return redirect('http://localhost:8000/oauth/authorize?'.$query);
+});
